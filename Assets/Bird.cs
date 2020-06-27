a3fe2514ab35a0ac24e8e55bd91c7f216dd4e352
Shubham Mhaske <shubham.mh96@gmail.com>
2020-06-27 21:44:21 +0530

    commit all

---
+++ Assets/Bird.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+﻿using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+    
+    [SerializeField] private float _launchPower = 500;
+
+    private void Awake()
+    {
+       _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+    
+       GetComponent<LineRenderer>().SetPosition(0, transform.position);
+       GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+       
+              
+       if (_birdWasLaunched &&
+	   GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+       {
+	   _timeSittingAround += Time.deltaTime;
+       }
+       
+       if (transform.position.y > 12 ||
+	   transform.position.y < -12 ||
+	   transform.position.x > 12 ||
+	   transform.position.x < -18 ||
+	   _timeSittingAround > 3)
+       {
+	   string currentSceneName = SceneManager.GetActiveScene().name;
+           SceneManager.LoadScene(currentSceneName);
+       }
+    }
+
+    private void OnMouseDown()
+    {
+       GetComponent<SpriteRenderer>().color = Color.red;
+       GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+       GetComponent<SpriteRenderer>().color = Color.white;
+       
+       Vector2 directionToInitialPosition = _initialPosition - transform.position;
+       
+       GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+       GetComponent<Rigidbody2D>().gravityScale = 1;
+       _birdWasLaunched = true;
+       
+       GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag()
+    {
+       Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);	
+       transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
---
+++ Assets/LevelController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+	private static int _nextLevelIndex = 1;
+	private Enemy[] _enemies;
+	
+	private void OnEnable()
+   {
+		_enemies = FindObjectsOfType<Enemy>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+   	foreach(Enemy enemy in _enemies)
+   	{
+   		if (enemy != null)
+   		return;
+   	}
+   	
+   	Debug.Log("You killed all enemies");
+   	
+   	_nextLevelIndex++;
+   	string nextLevelName = "Level" + _nextLevelIndex;
+   	SceneManager.LoadScene(nextLevelName);
+   }
+}
